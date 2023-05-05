11daca1e858fe900a049c3a19f62e41112f23693
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-05-06 00:28:54 +0700

    Add Navigation To SignIn Page

--- app/src/main/java/com/example/cinemax/SignInActivity.java
+++ app/src/main/java/com/example/cinemax/SignInActivity.java
@@ -2,7 +2,10 @@ package com.example.cinemax;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button signup_btn = (Button) findViewById(R.id.signup_btn);
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
