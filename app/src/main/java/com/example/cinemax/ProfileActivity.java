77f3d442a390281f7eca689eda16d6ea222b6c61
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-05-08 12:56:13 +0700

    Create Setting and Change City Screen

--- app/src/main/java/com/example/cinemax/ProfileActivity.java
+++ app/src/main/java/com/example/cinemax/ProfileActivity.java
@@ -31,5 +31,14 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(new Intent(v.getContext(), ProfileSettingActivity.class));
             }
         });
+
+        AppCompatButton changeCityBtn = (AppCompatButton) findViewById(R.id.change_city_btn);
+        changeCityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), ChangeCityActivity.class));
+            }
+        });
+
     }
 }
