bfde66580d51d3d91d297197cb91314511272bc8
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-05-09 16:12:23 +0700

    Checkout Screen

--- app/src/main/java/com/example/cinemax/CheckoutActivity.java
+++ app/src/main/java/com/example/cinemax/CheckoutActivity.java
@@ -5,20 +5,19 @@ import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
 
-public class SelectDateActivity extends AppCompatActivity {
+public class CheckoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_date);
+        setContentView(R.layout.activity_checkout);
 
         ImageButton backBtnSetting = (ImageButton) findViewById(R.id.back_btn_setting);
         backBtnSetting.setOnClickListener(v -> startActivity(new Intent(v.getContext(), DetailMovieActivity.class)));
 
-        AppCompatButton bookingBtn = (AppCompatButton) findViewById(R.id.checkout_btn);
-        bookingBtn.setOnClickListener(v -> startActivity(new Intent(v.getContext(), CheckoutActivity.class)));
+        AppCompatButton bookingBtn = (AppCompatButton) findViewById(R.id.pay_btn);
+        bookingBtn.setOnClickListener(v -> startActivity(new Intent(v.getContext(), ThanksActivity.class)));
     }
 }
