5968a2477855f9c32f0b28de61fb1f84ed2fa980
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-05-09 15:17:20 +0700

    Select Seat Screen

--- app/src/main/java/com/example/cinemax/DetailMovieActivity.java
+++ app/src/main/java/com/example/cinemax/DetailMovieActivity.java
@@ -1,6 +1,7 @@
 package com.example.cinemax;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,6 +20,14 @@ public class DetailMovieActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(v.getContext(), ContainerMainActivity.class));
+            }
+        });
+
+        AppCompatButton bookingBtn = (AppCompatButton) findViewById(R.id.booking_btn);
+        bookingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), SelectDateActivity.class));
             }
         });
     }
---
+++ app/src/main/java/com/example/cinemax/SelectDateActivity.java
@@ -0,0 +1,19 @@
+package com.example.cinemax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+public class SelectDateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_date);
+    }
+}
